44ea8dee36302000f7a668aa94f8ee1a0ee4acee
RickCole21 <rickcole21@outlook.com>
2021-03-05 11:17:23 +0000

    Deploying to gh-pages from  @ 62dea4530341e187dea1c0bcc31580059b07d00d 🚀

--- pkg/examples/mobile.html_map_8f0204e.js
+++ pkg/examples/mobile.html_map_8f0204e.js
@@ -9692,7 +9692,7 @@ amis.require.resourceMap({
       "type": "js"
     },
     "p7": {
-      "url": "/amis/pkg/app_907d9b3.js",
+      "url": "/amis/pkg/app_77787b5.js",
       "type": "js"
     },
     "p1": {
